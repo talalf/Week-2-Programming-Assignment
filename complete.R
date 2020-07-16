0e061c2e7cef4ecb73510867ba21ce777c19dd1b
talalf <talal.fazmin1@gmail.com>
2020-07-16 01:14:20 +0100

    "Complete" function for second part of assignment. This function lists all the sensor ids (id) and number of complete observations (nobs) for each id.

--- complete.R
+++ complete.R
@@ -1,5 +1,18 @@
 complete <- function(directory, id = 1:332) {
-  
-  
-  
-}
+  nobs <- c()
+  for(i in id){
+    if(i >= 100){
+      filepath <- paste(directory, "/", i, ".csv", sep = "")
+      }
+    else if(i >= 10) {
+      filepath <- paste(directory, "/", "0", i, ".csv", sep = "")
+      }
+    else {
+      filepath <- paste(directory, "/", "00", i, ".csv", sep = "")
+      }
+  obs <- sum(complete.cases(read.csv(filepath)))
+  nobs <- append(nobs, obs)
+  }
+  outputframe <- data.frame(id, nobs)
+  outputframe
+  }
